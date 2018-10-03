3795800f2cb935afb46806a6cffe8ec0f7ffe734
kliangh <kunliang.hou@gmail.com>
2018-10-03 10:32:19 +0800

    Initial commit

---
+++ src/code/kliangh/leetcode/solution/TwoSum.java
@@ -0,0 +1,24 @@
+package code.kliangh.leetcode.solution;
+
+public class TwoSum {
+    public int[] findTwoSumIndexes(int[] nums, int target) {
+        //        int nums[] = new int[] {2,7,11,15};
+        //        int target = 22;
+
+        for (int i = 0; i < nums.length; i++) {
+            int remaining = target - nums[i];
+            int indexOfRemaining;
+
+            if (IntStream.of(nums).anyMatch(e -> e == remaining)) {
+                for (int j = 0; j < nums.length; j++) {
+                    if (nums[j] == remaining && j != i) {
+                        indexOfRemaining = j;
+                        return new int[] {i, indexOfRemaining};
+                    }
+                }
+            }
+        }
+        return null;
+
+    }
+}
